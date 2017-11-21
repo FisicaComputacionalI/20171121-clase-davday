5553d91ef1f8e15ae2985bd42e4b54ccf26308d0
David Israel Pacheco Romero <31254482+davday@users.noreply.github.com>
2017-11-21 13:55:28 -0600

    Add files via upload
    Solo cálcula cos

---
+++ cos.cc
@@ -0,0 +1,12 @@
+//cos
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+  cout<< "intriduce el ángulo(en radianes)";
+  double x;
+  cin>>x;
+  double coseno=cos(x);
+  cout<<"el coseno de"<<x<<"es"<<coseno<<endl;
+  return 0;
+}
